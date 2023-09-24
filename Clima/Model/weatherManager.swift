af7e0a42677e9f0ad45cfa20d7e2a61eebc183a3
aronbergman <brgmn@icloud.com>
2023-09-24 13:12:18 +0200

    feat(view): created url session

--- Clima/Model/weatherManager.swift
+++ Clima/Model/weatherManager.swift
@@ -12,8 +12,34 @@ struct WeatherManager {
     let apiKey = "bb6604b1269d65e5e050234364f52d9b"
     
     func fetchWeather(cityName: String) {
-        let urlForFetch = "https://api.openweathermap.org/data/2.5/weather?q=\(cityName)&appid=\(apiKey)"
+        let city = cityName.trimmingCharacters(in: .whitespacesAndNewlines)
+        let urlForFetch = "https://api.openweathermap.org/data/2.5/weather?q=\(city)&appid=\(apiKey)"
         
-        print(urlForFetch)
+        
+        performRequest(urlString: urlForFetch)
+    }
+    
+    func performRequest(urlString: String) {
+        // 1. Create URL
+        if let url = URL(string: urlString) {
+            // 2. Create session with configuration
+            let session = URLSession(configuration: .default)
+            // 3. Give the session a task
+            let task = session.dataTask(with: url, completionHandler: handle)
+            // 4. Start the task
+            task.resume()
+        }
+    }
+    
+    func handle(data: Data?, response: URLResponse?, error: Error?) {
+        if error != nil {
+            print("❌: \(error!)")
+            return
+        }
+        
+        if let safeData = data {
+            let dataString = String(data: safeData, encoding: .utf8)
+            print(dataString)
+        }
     }
 }
